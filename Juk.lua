2321eb51b2176e4f421fc83ffe375750be5bf09c
arakeaty-ctrl <arakeaty@gmail.com>
2025-12-23 13:36:26 +0700

    Create Juk.lua

---
+++ Juk.lua
@@ -0,0 +1,61 @@
+-- สร้าง ScreenGui
+local gui = Instance.new("ScreenGui")
+gui.Name = "LionLogoGUI"
+gui.Parent = game.CoreGui
+
+-- สร้างปุ่มโลโก้สิงโต
+local lionButton = Instance.new("TextButton")
+lionButton.Size = UDim2.new(0, 100, 0, 100) -- ขนาด 100x100
+lionButton.Position = UDim2.new(0, 20, 0, 20) -- ตำแหน่งบนหน้าจอ
+lionButton.BackgroundColor3 = Color3.fromRGB(0,0,0) -- พื้นหลังสีดำ
+lionButton.Text = "🦁" -- ใช้อิโมจิสิงโต
+lionButton.TextScaled = true -- ขยายข้อความให้เต็มปุ่ม
+lionButton.TextColor3 = Color3.fromRGB(255, 0, 0) -- สีแดง
+lionButton.Parent = gui
+
+-- กดปุ่มทำอะไรได้ (ตัวอย่าง)
+lionButton.MouseButton1Click:Connect(function()
+    print("โลโก้สิงโตถูกกด!")
+    -- คุณสามารถเรียกเปิดเมนูหรือสคริปต์อื่นตรงนี้ได้
+end)
+repeat wait() until game:IsLoaded() and game:FindFirstChild("CoreGui") and pcall(function() return game.CoreGui end)
+
+getgenv().jinkX = getgenv().jinkX หรือ {}
+ฟังก์ชันภายใน = {
+["block_executor"] = ฟังก์ชัน()
+ผู้ดำเนินการที่ถูกบล็อกในพื้นที่ = {
+"ซีโน"
+"โซลาร่า"
+"ความเร็ว"
+-
+ถ้า identifyexecutor แล้ว
+กระแสไฟฟ้าภายในเครื่อง = identifyexecutor():lower()
+สำหรับ _, ชื่อใน ipairs(blockedExecutors) ทำ
+ถ้า current:find(name) แล้ว
+ส่งคืนค่าจริง
+จบ
+จบ
+จบ
+ส่งคืนค่าเท็จ
+จบ,
+["getid"] = ฟังก์ชัน()
+g ในเครื่อง = game.GameId
+ถ้า getgenv().jinkX และ getgenv().jinkX["Fish It"] และ getgenv().jinkX["Fish It"]["Enabled"] แล้วให้ส่งคืน "0708c0109bad431c5513f6d2dcc9b9e5" end -- Fish It Kaitun
+
+ถ้า g == 6701277882 แล้วให้ส่งคืน "e0f5b1e2af8eb6900e1324bef4e472b7" -- Fish It  
+    elseif g == 8009328211 then return "bf9e3c7d3db39fba6940d81c8eddedf8" -- Raise Animal  
+    elseif g == 9051406594 then return "d73b977af0e75026f30f65e1e6c27538" -- Dueling Grounds  
+    elseif g == 6739698191 then return "f1b45d9a1002a09f0c3aaba884b1c236" -- Violence District  
+    elseif g == 8974769492 then return "4afee01bcf9ffe2c814e3c5eb3296888" -- Arise Ragnarok  
+    จบ  
+จบ,  
+["gamename"] = ฟังก์ชัน()  
+    g ในเครื่อง = game.GameId  
+    ถ้า g == 6701277882 ให้ส่งคืน "Fish It"  
+    elseif g == 8009328211 then return "Raise Animal"  
+    elseif g == 9051406594 then return "Dueling Grounds"  
+    elseif g == 6739698191 then return "Violence District"  
+    elseif g == 8974769492 then return "Arise Ragnarok"  
+    จบ  
+จบ,
+  
